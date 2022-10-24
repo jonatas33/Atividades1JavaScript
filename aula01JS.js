97c0c1f15f01a68b9b8bb0e8a8e5a70a9d4efd76
jonatas33 <116115754+jonatas33@users.noreply.github.com>
2022-10-24 16:59:04 -0300

    Add files via upload

---
+++ aula01JS.js
@@ -0,0 +1,5 @@
+window.alert("Olá Devs Senac")
+window.alert("Esta é a minha primeira mensagem")
+window.confirm("Está Gostando de Javascript")
+window.prompt("Qual o seu nome?")
+document.write("Estou escrevendo no corpo")
